d7169e1832cb8f9ccf72537479edc998300dc44e
CaptainJS <45124354+CaptainJavaScript@users.noreply.github.com>
2019-01-05 16:16:56 +0100

    Update usingCaptainJS_v2.sol

--- usingCaptainJS_v2.sol
+++ usingCaptainJS_v2.sol
@@ -19,14 +19,16 @@ contract ICaptainJS {
 
 contract usingCaptainJS {
 
-    address CaptainsAddress = 0xc52f52057f6bf5ecc75d704cb85510f4e1ba207b; // MAINNET
-    // address constant CaptainsAddressAtROPSTEN = 0x2cc6e8a4dd631fbb504e696231526b3a984c1e46;
+    address CaptainsAddress;
+    address constant CaptainsAddressAtMAINNET = 0xc52f52057f6bf5ecc75d704cb85510f4e1ba207b;
+    address constant CaptainsAddressAtROPSTEN = 0x2cc6e8a4dd631fbb504e696231526b3a984c1e46;
 
     uint constant DEFAULT_GAS_PRICE = 3000000000 wei; // 3 gwei
     uint constant DEFAULT_GAS_UNITS = 100000;
     bool HasVoucher = false;
 
     constructor () internal {
+        SetCaptainsAddress(CaptainsAddressAtROPSTEN);
     }
 
     function SetCaptainsAddress(address NewAddress) public {
